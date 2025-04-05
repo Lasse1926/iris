60410f1b472c722775e4c862697c420b34c1e74c
Lasse <206079734+Lasse1926@users.noreply.github.com>
2025-04-05 17:58:44 +0200

    implemented CIELab Color Space and Conversion from rgb

--- src/iris_color.rs
+++ src/iris_color.rs
@@ -54,7 +54,8 @@ impl AvarageRgb {
 
         AvarageRgb {r,g,b,color_n:1,texture:None}
     }
-    pub fn avarage(&mut self,comp: &AvarageRgb){
+
+    pub fn _avarage(&mut self,comp: &AvarageRgb){
         self.color_n += comp.color_n;
         self.r += comp.r/self.color_n as u8;
         self.g += comp.g/self.color_n as u8;
@@ -79,4 +80,69 @@ impl fmt::Display for AvarageRgb {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f,"({},{},{})",self.r,self.g,self.b)
     }
+}
+
+pub struct Vec3 {
+    x:f32,
+    y:f32,
+    z:f32,
+}
+
+pub const XYZ_D65:Vec3 = Vec3{x:95.047,y:100.0,z:108.883};
+
+pub struct LabColor {
+    l:f32,
+    a:f32,
+    b:f32,
+    x:f32,
+    y:f32,
+    z:f32,
+}
+
+impl LabColor {
+    pub fn new(l:f32,a:f32,b:f32)-> Self{
+        Self{l,a,b,x:XYZ_D65.x,y:XYZ_D65.y,z:XYZ_D65.z}
+    } 
+
+    pub fn from_XYZ(xyz:&Vec3) -> Self{
+        let mut var_x = xyz.x/XYZ_D65.x;
+        let mut var_y = xyz.y/XYZ_D65.y;
+        let mut var_z = xyz.z/XYZ_D65.z;
+
+        var_x = if var_x > 0.008856 {var_x.powf(1.0/3.0)} else {(7.787 * var_x)+(16.0/116.0)};
+        var_y = if var_y > 0.008856 {var_y.powf(1.0/3.0)} else {(7.787 * var_y)+(16.0/116.0)};
+        var_z = if var_z > 0.008856 {var_z.powf(1.0/3.0)} else {(7.787 * var_z)+(16.0/116.0)};
+
+        let cie_l = (116.0 * var_y) - 16.0;
+        let cie_a = 500.0 *(var_x - var_y);
+        let cie_b = 200.0 *(var_y - var_z);
+
+        Self::new(cie_l,cie_a,cie_b)
+    }
+}
+
+impl fmt::Display for LabColor {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f,"({},{},{})",self.l,self.a,self.b)
+    }
+}
+
+pub fn rgb_to_XYZ(rgb:&Rgb<u8>) -> Vec3 {
+    let mut r = rgb.channels()[0] as f32 /255.0;
+    let mut g = rgb.channels()[1] as f32 /255.0;
+    let mut b = rgb.channels()[2] as f32 /255.0;
+
+    r = if r > 0.04045 {((r + 0.055)/1.055).powf(2.4)} else {r/12.92};
+    g = if g > 0.04045 {((g + 0.055)/1.055).powf(2.4)} else {g/12.92};
+    b = if b > 0.04045 {((b + 0.055)/1.055).powf(2.4)} else {b/12.92};
+
+    r = r*100.0;
+    g = g*100.0;
+    b = b*100.0;
+
+    let x = r * 0.4124 + g * 0.3576 + b * 0.1805;
+    let y = r * 0.2126 + g * 0.7152 + b * 0.0722;
+    let z = r * 0.0193 + g * 0.1192 + b * 0.9505;
+
+    Vec3{x,y,z}
 }
