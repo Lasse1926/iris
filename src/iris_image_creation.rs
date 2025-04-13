aee8617717def889a36cb7d1162691b6371c7591
Lasse <206079734+Lasse1926@users.noreply.github.com>
2025-04-13 05:22:22 +0200

    RGB Rect Image Generation

--- src/iris_image_creation.rs
+++ src/iris_image_creation.rs
@@ -25,7 +25,7 @@ impl ImageCreator {
             let mut window_open = self.open;
             egui::Window::new("ImageCreator").id(egui::Id::new(self.id)).open(&mut window_open).show(ctx,|ui|{
                 if ui.add(egui::Button::new("gen")).clicked() {
-                    self.iris_gen();
+                    self.generate_rgb_image_rect();
                 }
             });
             self.open = window_open;
@@ -40,5 +40,55 @@ impl ImageCreator {
         }
         let _ = img.save("./created_images/color_rect.png");
     }
+    pub fn generate_rgb_image_rect(&self) {
+        let mut img = RgbImage::new(64,64);
+        for x in 0..64 {
+            let rgb = self.rgb_rect_y(x as f32 / 64.0);  
+
+            let mut rgb_u8:[u8;3]=[0,0,0];
+
+            rgb_u8[0] = (rgb[0] * 255.0) as u8;
+            rgb_u8[1] = (rgb[2] * 255.0) as u8;
+            rgb_u8[2] = (rgb[1] * 255.0) as u8;
+
+
+
+            for y in 0..64 {
+
+                rgb_u8[0] = (rgb[0] * 255.0) as u8;
+                rgb_u8[1] = (rgb[2] * 255.0) as u8;
+                rgb_u8[2] = (rgb[1] * 255.0) as u8;
+                if y > 32 {
+                    let step:f32  = 1.0 - ((y as f32 - 32.0)/32.0);
+                    rgb_u8[0] = (rgb_u8[0] as f32 * step) as u8;
+                    rgb_u8[1] = (rgb_u8[1] as f32 * step) as u8;
+                    rgb_u8[2] = (rgb_u8[2] as f32 * step) as u8;
+                }
+                if y < 32 {
+                    let step:f32  = f32::abs((y as f32 - 32.0)/32.0);
+                    rgb_u8[0] = (rgb_u8[0] as f32 + (255 - rgb_u8[0])as f32 * step) as u8;
+                    rgb_u8[1] = (rgb_u8[1] as f32 + (255 - rgb_u8[1])as f32 * step) as u8;
+                    rgb_u8[2] = (rgb_u8[2] as f32 + (255 - rgb_u8[2])as f32 * step) as u8;
+                    println!("y : {} ----------Step {}-------",y,step);
+                    println!("r: {}",rgb_u8[0]);
+                    println!("g: {}",rgb_u8[1]);
+                    println!("b: {}",rgb_u8[2]);
+                }
+                img.put_pixel(x, y, Rgb(rgb_u8));
+            }
+        }
+        let _ = img.save("./created_images/rgb_rect.png");
+    }
+    fn rgb_rect_y(&self,x:f32) -> [f32;3] {
+        let r = (-9.0*x.powf(2.0) + 3.0*x + 0.75).max(0.0);
+        let g = (-9.0*x.powf(2.0) + 9.0*x - 1.25).max(0.0);
+        let b = (-9.0*x.powf(2.0) + 15.0*x - 5.25).max(0.0);
+
+        let b_2 = (-9.0*x.powf(2.0) -3.0*x + 0.75).max(0.0); 
+        let r_2 = (-9.0*x.powf(2.0) + 21.0*x - 11.25).max(0.0);
+
+        [r + r_2,g,b + b_2]
+    }
 }
+
 
